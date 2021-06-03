ec60c5d6459c47f99edd9822c0543b613650fbe0
Emma Mattner <emattner1997@gmail.com>
2021-06-03 17:32:55 +0930

    adding hasMany

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,6 +11,9 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'catergory_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
