4a436aaa8ba8c54afb1f97ae3f5c7143cc806176
Emma Mattner <emattner1997@gmail.com>
2021-07-15 18:45:16 +0930

    updating routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,37 +61,50 @@ router.get('/', async (req, res) => {
       res.status(400).json(err);
     }
   });
-.then((category) => res.json(category))
-.catch((err) => {
-  console.log(err);
-  res.status(400).json(err);
-});
-});
 
 
-router.put('/:id', (req, res) => {
-  Category.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((category) => res.status(200).json(category))
-    .catch((err) => res.status(400).json(err));
-});
+  router.put('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.update(
+        {
+          category_name: req.body.category_name
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+      });
+      if (!categoryData) {
+        res.status(404).json({ message: 'Cannot update category with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
-
-router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.destroy({
-    where: {
-      id: req.params.id
-    },
-  })
-.then((category) => res.status(200).json(category))
-.catch((err) => 
-  res.status(400).json(err));
-});
+  router.delete('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 
 module.exports = router;
