ada8fbce631f5f9555d19b7e360f0a905ffee07f
Emma Mattner <emattner1997@gmail.com>
2021-07-16 18:36:04 +0930

    updating hasmany

--- models/index.js
+++ models/index.js
@@ -6,13 +6,14 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'catergory_id',
+  foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'catergory_id',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
