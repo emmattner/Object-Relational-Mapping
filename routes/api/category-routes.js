dc1583fbed87d98b987344fe86b94453094e0a84
Emma Mattner <emattner1997@gmail.com>
2021-07-19 16:15:41 +0930

    formatting document

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ const { Category, Product, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-    // Includes associated Products data
+      // Includes associated Products data
       include: [Product]
     });
     if (!categoryData) {
@@ -22,10 +22,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-  // find one category by its `id` value
-  router.get('/:id', async (req, res) => {
-    try {
-      const categoryData = await Category.findByPk(req.params.id, {
+// find one category by its `id` value
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
       // Includes associated Products data
       include: [
         {
@@ -33,73 +33,73 @@ router.get('/', async (req, res) => {
           attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
         }
       ]
-      });
-      if (!categoryData) {
-        res.status(404).json({ message: 'No categories found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found with this id!' });
+      return;
     }
-  });
 
-  // craete a new category
-  router.post('/', async (req, res) => {
-    try {
-      const categoryData = await Category.create({
-        category_name: req.body.category_name,
-      })
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-
-  router.put('/:id', async (req, res) => {
-    try {
-      const categoryData = await Category.update(
-        {
-          category_name: req.body.category_name
-        },
-        {
-          where: {
-            id: req.params.id
-          }
-      });
-      if (!categoryData) {
-        res.status(404).json({ message: 'Cannot update category with this id!' });
-        return;
-      }
-  
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// craete a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
-  // delete a category by its `id` value
-  router.delete('/:id', async (req, res) => {
-    try {
-      const categoryData = await Category.destroy({
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
         where: {
           id: req.params.id
         }
       });
-      if (!categoryData) {
-        res.status(404).json({ message: 'No category found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
+    if (!categoryData) {
+      res.status(404).json({ message: 'Cannot update category with this id!' });
+      return;
     }
-  });
-  
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 
 module.exports = router;
